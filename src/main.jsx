113a20898fa7750fa241d53dd13d1f459ac1e84e
nir.avraham@perimeterx.com <nir.avraham@perimeterx.com>
2022-06-20 19:56:00 +0300

    add sw

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js");
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
