d6bb2019424a6c4d9dcaef753e9501d7833aa788
nir.avraham@perimeterx.com <nir.avraham@perimeterx.com>
2022-08-07 21:30:34 +0300

    Update analytics.js

--- src/app/services/analytics.js
+++ src/app/services/analytics.js
@@ -1,5 +1,11 @@
-import { logEvent } from "firebase/analytics";
+import { logEvent as fbLogEvent } from "firebase/analytics";
 import { analytics } from "./firebase";
+
+const isLocalhost = window.location.hostname === "localhost";
+
+const logEvent = isLocalhost
+  ? (_, eventName) => console.log("analytics event:", eventName)
+  : fbLogEvent;
 
 export const clickAddNewItem = () => logEvent(analytics, "add_new_item");
 
